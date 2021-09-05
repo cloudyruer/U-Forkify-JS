aab8433f87266a5600afaf5ff6e97ace13310979
Joey Chou <cloudyruer@gmail.com>
2021-09-05 16:14:54 +0800

    fix fractional issue

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,8 @@
 import View from './View.js';
 // import icons from '../img/icons.svg'; //Parcel 1
 import icons from 'url:../../img/icons.svg'; //Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional'; //用來顯示分數 但有問題 所以改成下方的
+import { numberToFraction } from '../helper.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -133,7 +134,8 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            // ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            ing.quantity ? numberToFraction(ing.quantity).toString() : ''
           }</div>
           <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
