db10d7f4247d51d25e905046b011b48d3a50146a
Joey Chou <cloudyruer@gmail.com>
2021-09-05 16:36:43 +0800

    Added note and comment

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '6d5f685c-9771-46db-8b7d-7385cbf3ef0b';
+export const KEY = '6d5f685c-9771-46db-8b7d-7385cbf3ef0b'; // NOTE 展示作品用 此為練習用免洗KEY 😎
 export const MODAL_CLOSE_SEC = 2.5;
